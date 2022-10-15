776ba569947010d09f38a63c6c5191263d3fa329
Akshata S H <akshatash@microsoft.com>
2022-10-15 21:00:24 +0530

    Latest changes adding controller Signed-off-by: Akshata S H <akshatash@microsoft.com>

---
+++ log4jwithAI/src/main/java/com/log4jwithAI/HelloController.java
@@ -0,0 +1,14 @@
+package com.log4jwithAI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from New Spring Boot App- Hi ASH!";
+	}
+
+}
